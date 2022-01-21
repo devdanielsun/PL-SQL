e2eb9ec65689d9459b73e917ec3144f88f58803e
DevDaniëls <daniel.geerts@live.nl>
2022-01-21 14:19:14 +0100

    Create delimiters.sql

---
+++ delimiters.sql
@@ -0,0 +1,14 @@
+SET SERVEROUTPUT ON:
+
+------------------DELIMITERS AND COMMENTING------------------
+DECLARE
+V_TEXT VARCHAR2(10):= 'PL/SQL';
+BEGIN
+--This is a single line comment
+/* This is a 
+    multi line
+    comment */
+--DBMS_OUTPUT.PUT_LINE(V_TEXT || ' is a good language');
+null;
+END;
+-------------------------------------------------------------
