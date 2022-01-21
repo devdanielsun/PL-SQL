ef3657f839246b1a33199b95c164743c91f58070
DevDaniëls <daniel.geerts@live.nl>
2022-01-21 17:10:08 +0100

    Update bind-variables.sql

--- bind-variables.sql
+++ bind-variables.sql
@@ -1,13 +1,13 @@
 --------------------------BIND VARIABLES--------------------------
 set serveroutput on;
 set autoprint on;
-/
+
 variable var_text varchar2(30);
-/
+
 variable var_number NUMBER;
-/
+
 variable var_date DATE;
-/
+
 declare
 v_text varchar2(30);
 begin
@@ -17,15 +17,15 @@ v_text := :var_text;
 --dbms_output.put_line(v_text);
 --dbms_output.put_line(:var_text);
 end;
-/
+
 print var_text;
-/
+
 variable var_sql number;
-/
+
 begin 
   :var_sql := 100;
 end;
-/
+
 select * from employees where employee_id = :var_sql;
  
 -----NOTE: When you run a bind variable creation and select statement together, SQL Developer may return an error. But when you execute them separately, there will be no problem--------
